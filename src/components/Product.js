4c1c08fe2716583c7d39c6974bb4e55c4268c061
nickolay <nosenkomi02@gmail.com>
2022-11-12 20:11:54 +0200

    init shop-page-dev branch

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {Button, Card, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
 
-const Product = () => {
+const Product = ({name, description, price}) => {
     return (
-        <div>
-            
-        </div>
+        <Card sx={{ maxWidth: 345 }}>
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    {name}
+                </Typography>
+                <Typography variant="caption" color="text.secondary">
+                    {description}
+                </Typography>.
+                <Typography variant="body2" color="text.secondary">
+                    {price}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">To cart</Button>
+            </CardActions>
+        </Card>
     );
 };
 
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Context} from "../index";
+import {useCollectionData} from "react-firebase-hooks/firestore";
+import Loader from "./Loader";
 
 const Shop = () => {
+
+    // const{auth, firestore} = useContext(Context)
+    // const [message, loading] = useCollectionData(
+    //     firestore.collection("products").orderBy("name")
+    // )
+    //
+    // if (loading){
+    //     return <Loader/>
+    // }
+
     return (
         <div>
-            
+            Shop
         </div>
     );
 };
