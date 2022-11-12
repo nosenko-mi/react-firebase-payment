b1088fc6d2d193b6367cf191b7412d46a6ce770f
nickolay <nosenkomi02@gmail.com>
2022-11-12 20:10:41 +0200

    create saveUser for creating document in database

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Box, Button, Container, Grid} from "@mui/material";
 import "firebase/firestore"
+import {Context} from "../index";
+import firebase from "firebase/compat/app";
 
 const Login = () => {
 
-    // const {auth} = useContext(Context)
-    //
-    const login = async () => {
-        // const provider = new firebase.auth.GoogleAuthProvider()
-        // const {user} = await auth.signInWithPopup(provider)
-        // console.log(user)
+    const {auth, firestore} = useContext(Context)
+    // const [user] = useAuthState(auth)
 
-        console.log("clicked")
+    const login = async () => {
+        const provider = new firebase.auth.GoogleAuthProvider()
+        const {user} = await auth.signInWithPopup(provider)
+        await saveUser(user)
+        console.log(user)
+    }
+
+    //TODO create User, UserRepository
+    const saveUser = async (_user) => {
+        firestore.collection("users").doc(_user.email).set({
+            uid: _user.uid,
+            displayName: _user.displayName,
+            email: _user.email
+        }, {merge: true})
+            .then(() => {
+                console.log("Document successfully written!")
+            })
+            .catch(e => {
+            console.log(e)})
     }
 
     return (
