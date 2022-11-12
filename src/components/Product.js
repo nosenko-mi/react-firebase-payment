9bc14b606e33bbbbc1ea2e85f9dcaac89dc10c15
nickolay <nosenkomi02@gmail.com>
2022-11-12 21:45:04 +0200

    create shop page basic version

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import {Button, Card, CardActions, CardContent, CardMedia, Typography} from "@mui/material";
+import {Button, Card, CardActions, CardContent, Typography} from "@mui/material";
 
 const Product = ({name, description, price}) => {
     return (
-        <Card sx={{ maxWidth: 345 }}>
+        <Card sx={{ maxWidth: 200 }}>
             <CardContent>
+
                 <Typography gutterBottom variant="h5" component="div">
                     {name}
                 </Typography>
+
                 <Typography variant="caption" color="text.secondary">
                     {description}
-                </Typography>.
-                <Typography variant="body2" color="text.secondary">
-                    {price}
                 </Typography>
+
+                <Typography variant="body2" color="text.secondary">
+                    {price} UAH
+                </Typography>
+
             </CardContent>
             <CardActions>
                 <Button size="small">To cart</Button>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,22 +2,70 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 import {useCollectionData} from "react-firebase-hooks/firestore";
 import Loader from "./Loader";
+import {Box, Button, Container, Grid, Stack} from "@mui/material";
+import Product from "./Product";
 
 const Shop = () => {
 
-    // const{auth, firestore} = useContext(Context)
-    // const [message, loading] = useCollectionData(
-    //     firestore.collection("products").orderBy("name")
-    // )
-    //
-    // if (loading){
-    //     return <Loader/>
-    // }
+    const{auth, firestore} = useContext(Context)
+    const [products, loading] = useCollectionData(
+        firestore.collection("products")
+    )
+
+    if (loading){
+        return <Loader/>
+    }
 
     return (
-        <div>
-            Shop
-        </div>
+        // <Container>
+        //     <Box
+        //         display="flex"
+        //         justifyContent="center"
+        //         alignItems="center"
+        //         flexDirection="row"
+        //         minHeight="90vh"
+        //     >
+        //         <Stack direction="row" spacing={2}>
+        //             {products.map(p =>
+        //                 <Product key={p.name} name={p.name} description={p.description} price={p.price}/>
+        //             )}
+        //         </Stack>
+        //
+        //     </Box>
+        // </Container>
+
+        <Container>
+            <Box
+                // sx={{ flexGrow: 1 }}
+                flexGrow="1"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="90vh"
+            >
+                {/*<Grid container justifyContent="center" spacing={{ xs: 1, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>*/}
+                {/*    {products.map(p => (*/}
+                {/*        <Grid item xs={2} sm={4} md={4} key={p}>*/}
+                {/*            <Product key={p.name} name={p.name} description={p.description} price={p.price}/>*/}
+                {/*        </Grid>*/}
+                {/*    ))}*/}
+                {/*</Grid>*/}
+
+                <Grid
+                    container
+                    spacing={0}
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    {products.map(p => (
+                        <Grid item xs={3}>
+                            <Product key={p.name} name={p.name} description={p.description} price={p.price}/>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Box>
+        </Container>
     );
 };
 
