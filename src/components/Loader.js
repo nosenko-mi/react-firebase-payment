0b9583c18c941714cc43ff45278aaffb7513e305
nickolay <nosenkomi02@gmail.com>
2022-11-14 18:46:15 +0200

    use mui loader instead of custom one

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import {Box, Container, Grid} from "@mui/material";
-// import styles from 'src/styles/Loader.module.css';
-import './Loader.css';
+import {Box, CircularProgress} from "@mui/material";
 
 const Loader = () => {
     return (
-        <Container>
-            <Box
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-                minHeight="90vh"
-            >
-                <Grid style={{width:400, background: 'lightgray'}}
-                      container
-                      alignItems="center"
-                      justifyContent="center"
-                >
-                    <div className="ldsRing">
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
-                </Grid>
-            </Box>
-        </Container>
+        <Box
+            flexGrow="1"
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            minHeight="90vh"
+        >
+            <CircularProgress />
+        </Box>
     );
 };
 
