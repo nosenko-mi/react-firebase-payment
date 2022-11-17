52dc9230ad367628870d2b156af2d307dce03ed4
nickolay <nosenkomi02@gmail.com>
2022-11-17 21:25:12 +0200

    add history + register route

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -8,6 +8,8 @@ import Cart from "./Cart";
 import CheckoutStripePage from "../pages/checkoutStripePage";
 import SuccessPaymentPage from "../pages/successPaymentPage";
 import CancelCheckoutPage from "../pages/cancelCheckoutPage";
+import HistoryPage from "../pages/historyPage";
+import Register from "./Register";
 
 const AppRouter = () => {
     const {auth} = useContext(Context)
@@ -17,7 +19,7 @@ const AppRouter = () => {
             <Routes>
                 <Route key={"/shop"} path={"/shop"} element={<Shop />}/>
                 <Route key={"/cart"} path={"/cart"} element={<Cart />}/>
-                <Route key={"/history"} path={"/history"} element={<Cart />}/>
+                <Route key={"/history"} path={"/history"} element={<HistoryPage />}/>
                 <Route key={"/checkoutStripe"} path={"/checkoutStripe"} element={<CheckoutStripePage />}/>
                 <Route key={"/success"} path={"/success"} element={<SuccessPaymentPage />}/>
                 <Route key={"/cancel"} path={"/cancel"} element={<CancelCheckoutPage />}/>
@@ -29,8 +31,10 @@ const AppRouter = () => {
             <Routes>
                 <Route key={"/shop"} path={"/shop"} element={<Shop />}/>
                 <Route key={"/login"} path={"/login"} element={<Login />}/>
+                <Route key={"/register"} path={"/register"} element={<Register />}/>
                 <Route key={"/success"} path={"/success"} element={<SuccessPaymentPage />}/>
                 <Route key={"/cancel"} path={"/cancel"} element={<CancelCheckoutPage />}/>
+
                 <Route key={"/history"} path={"/history"} element={<Login />}/>
                 <Route key={"/cart"} path={"/cart"} element={<Login />}/>
                 <Route key={"*"} path={"*"} element={<Navigate to="/shop"/>}/>
