5c92e10d3fa486157e0339262a17eaf7c98405b5
nickolay <nosenkomi02@gmail.com>
2022-11-17 21:22:37 +0200

    add basic email + password authorization

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,48 @@
-import React, {useContext} from 'react';
-import {Box, Button, Container, Grid} from "@mui/material";
+import React, {useContext, useState} from 'react';
+import {Box, Button, Container, Grid, Stack, TextField, Typography} from "@mui/material";
 import "firebase/firestore"
 import {Context} from "../index";
 import firebase from "firebase/compat/app";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {Navigate, NavLink} from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
 
     const {auth, firestore} = useContext(Context)
-    // const [user] = useAuthState(auth)
+    const [user] = useAuthState(auth)
+    const [loginEmail, setLoginEmail] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
 
-    const login = async () => {
-        const provider = new firebase.auth.GoogleAuthProvider()
-        const {user} = await auth.signInWithPopup(provider)
-        await saveUser(user)
-        console.log(user)
+
+    if (user) {
+        return <Navigate to="/" />;
+    }
+
+    const signInGoogle = async () => {
+
+        try {
+            const provider = new firebase.auth.GoogleAuthProvider()
+            const {user} = await auth.signInWithPopup(provider)
+            await saveUser(user)
+            console.log(user)
+        } catch (e){
+            console.log(e);
+        }
+    }
+
+    const signInEmailPassword = async () => {
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth,
+                loginEmail,
+                loginPassword
+            );
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
+            alert(error.message)
+        }
     }
 
     //TODO create User, UserRepository
@@ -38,13 +67,36 @@ const Login = () => {
                 alignItems="center"
                 minHeight="90vh"
             >
-                <Grid style={{width:400, background: 'lightgray'}}
+                <Grid style={{width:400}}
                       container
                       alignItems="center"
                       justifyContent="center"
                 >
-                    <Box p={5}>
-                        <Button onClick={login} variant={"contained"}>Sign in with Google</Button>
+                    <Box textAlign="center">
+                        <Stack spacing={3}>
+
+                            <Stack spacing={1}>
+                                <TextField
+                                    id="email"
+                                    label="Email"
+                                    type="email"
+                                    placeholder="Email"
+                                    onChange={(e) => {
+                                    setLoginEmail(e.target.value);
+                                }} />
+
+                                <TextField   id="password" label="Password" type="password" placeholder="Password"           onChange={(event) => {
+                                    setLoginPassword(event.target.value);
+                                }}/>
+
+                                <Button onClick={signInEmailPassword}>Log in</Button>
+                            </Stack>
+
+                            <Typography variant="body2">Don't have an account? <NavLink to="/register">Create one</NavLink> </Typography>
+                            <Typography variant="body2">or</Typography>
+                            <Button onClick={signInGoogle} variant={"contained"}>Sign in with Google</Button>
+                        </Stack>
+
                     </Box>
                 </Grid>
             </Box>
