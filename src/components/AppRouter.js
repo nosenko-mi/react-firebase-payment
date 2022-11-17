4f91e14d5191030aa24ab85ae5dc876b11ee1e6a
nickolay <nosenkomi02@gmail.com>
2022-11-17 12:48:16 +0200

    add stripe checkout routes, history route

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -5,6 +5,9 @@ import Login from "./Login";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {Context} from "../index";
 import Cart from "./Cart";
+import CheckoutStripePage from "../pages/checkoutStripePage";
+import SuccessPaymentPage from "../pages/successPaymentPage";
+import CancelCheckoutPage from "../pages/cancelCheckoutPage";
 
 const AppRouter = () => {
     const {auth} = useContext(Context)
@@ -14,6 +17,10 @@ const AppRouter = () => {
             <Routes>
                 <Route key={"/shop"} path={"/shop"} element={<Shop />}/>
                 <Route key={"/cart"} path={"/cart"} element={<Cart />}/>
+                <Route key={"/history"} path={"/history"} element={<Cart />}/>
+                <Route key={"/checkoutStripe"} path={"/checkoutStripe"} element={<CheckoutStripePage />}/>
+                <Route key={"/success"} path={"/success"} element={<SuccessPaymentPage />}/>
+                <Route key={"/cancel"} path={"/cancel"} element={<CancelCheckoutPage />}/>
                 <Route key={"*"} path={"*"} element={<Navigate to="/shop"/>}/>
             </Routes>
         )
@@ -22,6 +29,10 @@ const AppRouter = () => {
             <Routes>
                 <Route key={"/shop"} path={"/shop"} element={<Shop />}/>
                 <Route key={"/login"} path={"/login"} element={<Login />}/>
+                <Route key={"/success"} path={"/success"} element={<SuccessPaymentPage />}/>
+                <Route key={"/cancel"} path={"/cancel"} element={<CancelCheckoutPage />}/>
+                <Route key={"/history"} path={"/history"} element={<Login />}/>
+                <Route key={"/cart"} path={"/cart"} element={<Login />}/>
                 <Route key={"*"} path={"*"} element={<Navigate to="/shop"/>}/>
             </Routes>
         )
