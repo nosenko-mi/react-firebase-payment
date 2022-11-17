868adadae2b29683ae89932f31ba8d13ef4f8f0d
nickolay <nosenkomi02@gmail.com>
2022-11-17 12:49:15 +0200

    init success payment page

---
+++ src/pages/successPaymentPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SuccessPaymentPage = () => {
+    return (
+        <div>
+            Success!
+        </div>
+    );
+};
+
+export default SuccessPaymentPage;
