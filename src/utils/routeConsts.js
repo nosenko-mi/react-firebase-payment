1e36c48860762bbb24c3661116df635b38aebb23
nickolay <nosenkomi02@gmail.com>
2022-11-20 19:14:47 +0200

    [feat] add routes

--- src/utils/routeConsts.js
+++ src/utils/routeConsts.js
@@ -1,5 +1,8 @@
 export const LOGIN_ROUTE = '/login'
-export const SHOP_ROUTE = '/shop'
+export const REGISTER_ROUTE = '/register'
+export const SHOP_ROUTE = '/'
 export const CART_ROUTE = '/cart'
 export const HISTORY_ROUTE = '/history'
 export const STRIPE_ROUTE = '/stripeCheckout'
+
+export const SUCCESS_ROUTE = '/success'
