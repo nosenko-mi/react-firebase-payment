3fd94fd502d770c573a00afde9ce937ece87b2f8
nickolay <nosenkomi02@gmail.com>
2022-11-30 21:54:05 +0200

    add a little of style

--- src/pages/successPaymentPage.js
+++ src/pages/successPaymentPage.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {Box, Container, Typography} from "@mui/material";
 
 const SuccessPaymentPage = () => {
     return (
-        <div>
-            Success!
-        </div>
+
+        <Container>
+            <Box
+                // sx={{ flexGrow: 1 }}
+                flexGrow="1"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="90vh"
+                py={20}
+            >
+                <Typography variant="h4">
+                    Success!
+                </Typography>
+            </Box>
+        </Container>
+
     );
 };
 
